07496ce16d15ac2f1bc192d473fcaec194822ccc
tarique.hussain <tarique.hussain@bookmyshow.com>
2020-04-04 11:09:20 +0530

    Updates

---
+++ precache-manifest.87d011458b14f97a357a0a626243595b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd356ecd18d193780a451d676bd5b6f0",
+    "url": "/scan-app/index.html"
+  },
+  {
+    "revision": "81e3fb2ce20a42310bef",
+    "url": "/scan-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "909b6115a22d633c0e17",
+    "url": "/scan-app/static/js/2.960cd9a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/scan-app/static/js/2.960cd9a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81e3fb2ce20a42310bef",
+    "url": "/scan-app/static/js/main.861d4fa3.chunk.js"
+  },
+  {
+    "revision": "ed9afc506d808ab07fae",
+    "url": "/scan-app/static/js/runtime-main.a1dc27d9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/scan-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
